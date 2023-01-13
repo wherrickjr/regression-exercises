9af88a6541405cc226ba2f0be84658aeb732a996
Bill Herrick <wherrickjr@gmail.com>
2023-01-13 09:23:31 -0600

    scaling exercises

--- wrangle.py
+++ wrangle.py
@@ -6,12 +6,12 @@ import seaborn as sns
 import numpy as np
 import env
 import os
-
+from sklearn.model_selection import train_test_split
 
 def get_connection(db, user=env.username, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def wrangle_zillow():
+def get_zillow_data():
     filename = 'zillow.csv'
 
     if os.path.isfile(filename):
@@ -21,10 +21,57 @@ def wrangle_zillow():
         taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 \
             where propertylandusetypeid = 261', get_connection('zillow'))
         df.to_csv(filename)
-        df = df.rename(columns = {'bedroomcnt':'bedrooms', 'bathroomcnt':'bathrooms', 'calculatedfinishedsquarefeet':'sqft',
-                    'yearbuilt':'year_built'})
-        df.drop_duplicates(inplace=True)
-        df = df.dropna()
         return df
 
+#wrangle.py
+
+def acquire_zillow():
+    '''
+    This function checks to see if zillow.csv already exists, 
+    if it does not, one is created
+    '''
+    #check to see if telco_churn.csv already exist
+    if os.path.isfile('zillow.csv'):
+        df = pd.read_csv('zillow.csv', index_col=0)
     
+    else:
+
+        #creates new csv if one does not already exist
+        df = get_zillow_data()
+        df.to_csv('zillow.csv')
+
+    return df
+
+def prep_zillow(df):
+    '''
+    This function takes in the zillow df
+    then the data is cleaned and returned
+    '''
+    #change column names to be more readable
+    df = df.rename(columns={'bedroomcnt':'bedrooms', 
+                          'bathroomcnt':'bathrooms', 
+                          'calculatedfinishedsquarefeet':'area',
+                          'taxvaluedollarcnt':'tax_value', 
+                          'yearbuilt':'year_built'})
+
+    #drop null values- at most there were 9000 nulls (this is only 0.5% of 2.1M)
+    df = df.dropna()
+
+    #drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test
+
+
+def wrangle_zillow():
+    '''
+    This function uses the acquire and prepare functions
+    and returns the split/cleaned dataframe
+    '''
+    train, validate, test = prep_zillow(acquire_zillow())
+    
+    return train, validate, test
